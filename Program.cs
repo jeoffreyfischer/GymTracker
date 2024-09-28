67fa09ce75d0ac3a123e62cd87a6c507c27d4f65
Jeoffrey Fischer <jeoffrey.fischer@gmail.com>
2024-09-28 22:19:10 +1000

    Removed Weather Forecast

--- Program.cs
+++ Program.cs
@@ -6,7 +6,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<GymContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("GymDatabase")));
 
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
