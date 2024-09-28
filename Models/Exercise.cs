2c90c126aa11304e99ce4bec79c90e52db950081
Jeoffrey Fischer <jeoffrey.fischer@gmail.com>
2024-09-28 21:09:06 +1000

    Added models

--- Models/Exercise.cs
+++ Models/Exercise.cs
@@ -2,5 +2,8 @@
 {
     public class Exercise
     {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public ICollection<TrackingEntry>? TrackingEntries { get; set; }
     }
 }
--- Models/TrackingEntry.cs
+++ Models/TrackingEntry.cs
@@ -2,5 +2,13 @@
 {
     public class TrackingEntry
     {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public int LoadInKg { get; set; }
+        public int Reps { get; set; }
+        public int Sets { get; set; }
+        public int ExerciseId { get; set; }
+        public required Exercise Exercise { get; set; }
     }
+
 }
