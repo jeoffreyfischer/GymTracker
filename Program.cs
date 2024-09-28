3da4d430b05ef709ed01db085a68dd59d712f6c0
Jeoffrey Fischer <jeoffrey.fischer@gmail.com>
2024-09-28 21:13:30 +1000

    Added Db context in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<GymContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("GymDatabase")));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
