e23be041a5e8078cab616d5f1f10c994af7a27c1
Jeoffrey Fischer <jeoffrey.fischer@gmail.com>
2024-10-03 19:24:54 +1000

    Added CORS

--- gym-tracker-api/Program.cs
+++ gym-tracker-api/Program.cs
@@ -3,6 +3,17 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
+
 builder.Services.AddDbContext<GymContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("GymDatabase")));
 
@@ -13,6 +24,8 @@ builder.Services.AddScoped<ExerciseService>();
 builder.Services.AddScoped<TrackingEntryService>();
 
 var app = builder.Build();
+
+app.UseCors("AllowAllOrigins");
 
 if (app.Environment.IsDevelopment())
 {
