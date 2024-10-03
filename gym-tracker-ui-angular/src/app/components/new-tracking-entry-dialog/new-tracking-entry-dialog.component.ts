df724681f187bee0227ac7b5ed301779a32240c0
Jeoffrey Fischer <jeoffrey.fischer@gmail.com>
2024-10-03 19:02:49 +1000

    UI - Created the Add Entry feature

--- gym-tracker-ui-angular/src/app/components/new-tracking-entry-dialog/new-tracking-entry-dialog.component.ts
+++ gym-tracker-ui-angular/src/app/components/new-tracking-entry-dialog/new-tracking-entry-dialog.component.ts
@@ -1,11 +1,13 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-new-tracking-entry-dialog',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './new-tracking-entry-dialog.component.html',
   styleUrls: ['./new-tracking-entry-dialog.component.css']
 })
@@ -20,13 +22,18 @@ export class NewTrackingEntryDialogComponent {
     @Inject(MAT_DIALOG_DATA) public data: { selectedExerciseName: string }
   ) {
     const today = new Date();
-    this.date = today.toISOString().substring(0, 10); // Default to today
+    this.date = today.toISOString().substring(0, 10);
     this.load = 0;
     this.reps = 0;
     this.sets = 0;
   }
 
   save() {
+    if (this.load < 0 || this.reps < 0 || this.sets < 0) {
+      console.error("Load, Reps, and Sets must be 0 or more.");
+      return;
+    }
+
     this.dialogRef.close({
       date: this.date,
       load: this.load,
